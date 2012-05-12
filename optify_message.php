caa25c62947a573848ca1b7ac702a64f783ad948
TommyOptify <TommyOptify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-12 01:09:43 +0000

    tagging Optify for Wordpress version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/optify-for-wordpress/tags/1.0@543169 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ optify_message.php
@@ -0,0 +1,17 @@
+<?php
+function optify_message(){
+   global $wpdb;
+   $table_name = $wpdb->prefix . "optify_form";
+   $res = $wpdb->get_row("SELECT * FROM $table_name ");
+   echo '<div class="wrap">';
+   echo $res->optify_email.'&URL=http://'.$_SERVER['HTTP_HOST'].'&first_name=';
+   echo $res->optify_fname.'&last_name=';
+   echo $res->optify_lname.'&phone=';
+   echo $res->optify_phone.'&password=';
+   echo $res->optify_setpwd.'=wordpress';
+   echo '</div>';
+   exit();
+}
+optify_message();
+?>
+
