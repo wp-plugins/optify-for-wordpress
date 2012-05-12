cdb0f033876b1e4e88411fb43b811bb9ef6de0e6
TommyOptify <TommyOptify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-12 04:34:31 +0000

    A few cosmetic changes to the Wordpress listing
    git-svn-id: https://plugins.svn.wordpress.org/optify-for-wordpress/tags/1.0.1@543217 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/optify_script.js
@@ -0,0 +1,70 @@
+function validation(){
+
+      var fname = document.getElementById('fname').value;
+      if(trim(fname) == null || trim(fname) == ''){
+          document.getElementById("error-msz-fname").style.display = "block";
+          return false;
+      }
+      else{
+          document.getElementById("error-msz-fname").style.display = "none";
+      }
+
+      var lname = document.getElementById('lname').value;
+      if(trim(lname) == null || trim(lname) == ''){
+          document.getElementById("error-msz-lname").style.display = "block";
+          return false;
+      }
+      else{
+          document.getElementById("error-msz-lname").style.display = "none";
+      }
+
+      var phone = document.getElementById('phone').value;
+      if(trim(phone) == null || trim(phone) == ''){
+          document.getElementById("error-msz-phone").style.display = "block";
+          return false;
+      } else if(isNaN(phone.replace(/[^0-9]+/g, ""))){
+          document.getElementById('error-msz-phone').innerHTML = 'Phone Number must be Numeric';
+          document.getElementById("error-msz-phone").style.display = "block";
+          return false;
+      }
+      else{
+          document.getElementById("error-msz-phone").style.display = "none";
+      }
+
+      var email = document.getElementById('email').value;
+      var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if(trim(email) == null || trim(email) == ''){
+          document.getElementById("error-msz-email").style.display = "block";
+          return false;
+      } else if(!emailRegEx.test(email)){
+          document.getElementById('error-msz-email').innerHTML = 'Email is Required';
+          document.getElementById("error-msz-email").style.display = "block";
+          return false;
+      }
+      else{
+          document.getElementById("error-msz-email").style.display = "none";
+      }
+
+      var setpwd = document.getElementById('set-pwd').value;
+      if(trim(setpwd) == null || trim(setpwd) == ''){
+          document.getElementById("error-msz-pwd").style.display = "block";
+          return false;
+      } else if(setpwd.length <= 6){
+          document.getElementById('error-msz-pwd').innerHTML = 'Password minimum 6 character';
+          document.getElementById("error-msz-pwd").style.display = "block";
+          return false;
+      }
+      else{
+          document.getElementById("error-msz-pwd").style.display = "none";
+      }
+  }
+
+  function trim(s)
+{
+	var l=0; var r=s.length -1;
+	while(l < s.length && s[l] == ' ')
+	{	l++; }
+	while(r > l && s[r] == ' ')
+	{	r-=1;	}
+	return s.substring(l, r+1);
+}
---
+++ optify_form.php
@@ -0,0 +1,44 @@
+<div class="wrap">
+    <div class="optify-main-hedaer"><span class="optify-header-title"><?php echo '<h2>'._('Optify for Wordpress').'</h2>';?></span></div><div class="optify-error"><?php echo $optify_error_message; ?></div><div class="clear"></div>
+      <div class="optify-subheader">
+         <span class="optify-subheader-title"><?php echo '<h4>'._('Register for Optify to begin tracking your visitors, leads and SEO! ').'</h4>';?></span>
+      </div>
+      <div class="optify-form">
+         <form name="optifyform" method="post" action="" onsubmit="return validation();">
+             <input class="optify-inbox-box" type="hidden"  name="optify_token" value="Y" size="20">
+            <div class="optify-label-input">
+                <div class="optify-label"><?php _e("First Name" ); ?><span class="optif-star">*</span></div>
+                <div class="optify-input"><input class="optify-inbox-box" id="fname" type="text" name="optify_fname" value="" size="20"></div>
+                <div class="error123"><span class="error-msz" id="error-msz-fname">This is Required</span></div>
+                <div class="clear"></div>
+            </div>
+            <div class="optify-label-input">
+                <div class="optify-label"><?php _e("Last Name" ); ?><span class="optif-star">*</span></div>
+                <div class="optify-input"><input class="optify-inbox-box" id="lname" type="text" name="optify_lname" value="" size="20"></div>
+                <div class="error123"><span class="error-msz" id="error-msz-lname">This is Required</span></div>
+                <div class="clear"></div>
+            </div>
+            <div class="optify-label-input">
+                <div class="optify-label"><?php _e("Phone" ); ?><span class="optif-star">*</span></div>
+                <div class="optify-input"><input class="optify-inbox-box" id="phone" type="text" name="optify_phone" value="" size="20"></div>
+                <div class="error123"><span class="error-msz" id="error-msz-phone">This is Required</span></div>
+                <div class="clear"></div>
+            </div>
+            <div class="optify-label-input">
+                <div class="optify-label"><?php _e("Email" ); ?><span class="optif-star">*</span></div>
+                <div class="optify-input"><input class="optify-inbox-box" id="email" type="text" name="optify_email" value="" size="20"></div>
+                <div class="error123"><span class="error-msz" id="error-msz-email">This is Required</span></div>
+                <div class="clear"></div>
+            </div>
+            <div class="optify-label-input">
+                <div class="optify-label"><?php _e("Set your password" ); ?><span class="optif-star">*</span></div>
+                <div class="optify-input"><input class="optify-inbox-box" id="set-pwd" type="password" name="optify_setpwd" value="" size="20"></div>
+                <div class="error123"><span class="error-msz" id="error-msz-pwd">This is Required</span></div>
+                <div class="clear"></div>
+            </div>
+             <div class="optify-submit">
+                 <input class="optify-submit-button" type="submit" name="submit" value="<?php _e('Register for Optify!' ) ?>" />
+             </div>
+         </form>
+     </div>
+ </div>
